281634a6455a2cacd1e46d5c072c822b7ca9a5a3
Abid Rakhmansyah <bidnth@gmail.com>
2025-02-05 03:15:27 +0700

    feat: fix: fix build

--- src/app/(frontend)/tenant-slugs/login/page.tsx
+++ src/app/(frontend)/tenant-slugs/login/page.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
 import { Login } from '@/app/components/Login/client.page'
+import { headers } from 'next/headers'
 
-type RouteParams = {
-  tenant: string
-}
+export default async function Page() {
+  const headersList = headers()
+  const host = (await headersList).get('host') || ''
+  const tenantDomain = host.split(':')[0] // Remove port if exists
 
-export default async function Page({ params: paramsPromise }: { params: Promise<RouteParams> }) {
-  const params = await paramsPromise
-
-  return <Login tenantSlug={params.tenant} />
+  return <Login tenantDomain={tenantDomain} />
 }
