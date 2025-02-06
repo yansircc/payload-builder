d9aec140489283caf66553fff4aec1b644fc99ee
Abid Rakhmansyah <bidnth@gmail.com>
2025-02-06 14:00:36 +0700

    feat: fix: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -42,7 +42,7 @@ export default async function middleware(req: NextRequest) {
 
   // rewrite root application to `/home` folder
   if (hostname === 'localhost:3000' || hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN) {
-    return NextResponse.rewrite(new URL(`/home${path === '/' ? '' : path}`, req.url))
+    return NextResponse.rewrite(new URL(req.url))
   }
 
   // rewrite everything else to `/tenant-domains/[domain]/[path] dynamic route
