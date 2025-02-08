7d94566fbdf174e114566f6ea9be9963b8f13dd8
Penn York <markyan@foxmail.com>
2025-02-09 01:11:00 +0800

    fix: missing layout render

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -10,11 +10,13 @@ import { InitTheme } from '@/providers/Theme/InitTheme'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
 import { cn } from '@/utilities/ui'
 import './globals.css'
+import { CustomCodeComponent } from '@/CustomCode/Component'
 import { RenderHeader } from '@/Header/RenderHeader'
 import { getServerSideURL } from '@/utilities/getURL'
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const { isEnabled } = await draftMode()
+  const customCode = await CustomCodeComponent()
 
   return (
     <html className={cn(GeistSans.variable, GeistMono.variable)} lang="en" suppressHydrationWarning>
@@ -22,6 +24,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <InitTheme />
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
         <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+        {customCode?.headScripts}
       </head>
       <body>
         <Providers>
@@ -32,7 +35,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           />
 
           <RenderHeader />
+          {customCode?.bodyStartScripts}
           {children}
+          {customCode?.bodyEndScripts}
           <RenderFooter />
         </Providers>
       </body>
