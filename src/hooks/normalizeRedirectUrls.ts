601dac6d4cd0f12ef8a5640c0d1b9d73fb687bf0
Abid Rakhmansyah <bidnth@gmail.com>
2025-02-12 04:07:19 +0700

    fix: avoid special character

--- src/hooks/normalizeRedirectUrls.ts
+++ src/hooks/normalizeRedirectUrls.ts
@@ -1,5 +1,21 @@
+import { ValidationError } from 'payload'
+
 const normalizeUrl = (value: string): string => {
   if (!value) return value
+
+  // Check for special characters using a regex
+  const specialCharsRegex = /[!@#$%^&*()_+\=\[\]{};':"\\|,<>?~]/
+  if (specialCharsRegex.test(value)) {
+    throw new ValidationError({
+      errors: [
+        {
+          message: `Special characters are not allowed in redirect URLs`,
+          path: 'from',
+        },
+      ],
+    })
+  }
+
   try {
     const urlObj = new URL(value, 'http://x')
     return urlObj.pathname
