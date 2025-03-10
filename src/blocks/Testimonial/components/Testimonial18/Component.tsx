1da4b414929eb773b09d1457074839d3440b35dd
Arya Rizal <aryachubby27@gmail.com>
2025-03-10 20:26:49 +0700

    fix(TeamComponent): fix bugs Hide image Testimonial 18

--- src/blocks/Testimonial/components/Testimonial18/Component.tsx
+++ src/blocks/Testimonial/components/Testimonial18/Component.tsx
@@ -20,14 +20,12 @@ export default function Testimonial18({
     <section className="py-32">
       <div className="container">
         <div className="flex flex-col items-center gap-4">
-          <div className="flex items-center gap-1 text-sm font-semibold text-muted-foreground">
+          <div className="flex items-center gap-1 text-sm font-semibold">
             <Zap className="h-6 w-auto fill-primary" />
             {statsText}
           </div>
-          <h2 className="text-center text-3xl font-semibold lg:text-4xl text-muted-foreground">
-            {heading}
-          </h2>
-          <p className="text-center text-muted-foreground lg:text-lg">{subheading}</p>
+          <h2 className="text-center text-3xl font-semibold lg:text-4xl">{heading}</h2>
+          <p className="text-center text-foreground lg:text-lg">{subheading}</p>
         </div>
         <div className="mx-auto mt-20 max-w-screen-lg rounded-2xl bg-muted p-6 md:p-20">
           <div className="mb-6 flex gap-1">
@@ -35,8 +33,8 @@ export default function Testimonial18({
               <Star key={i} className="size-5 fill-amber-500 text-amber-500" />
             ))}
           </div>
-          <q className="text-2xl font-semibold md:text-4xl text-foreground">{quote}</q>
-          <p className="mt-6 text-foreground">{description}</p>
+          <q className="text-2xl font-semibold md:text-4xl">{quote}</q>
+          <p className="mt-6 text-muted-foreground">{description}</p>
           <div className="mt-6 flex gap-4">
             {!hideAuthorImages && (
               <Avatar className="size-14 rounded-full ring-1 ring-input overflow-hidden">
@@ -50,8 +48,8 @@ export default function Testimonial18({
               </Avatar>
             )}
             <div>
-              <p className="font-medium text-foreground">{authorName}</p>
-              <p className="text-sm text-foreground">{authorRole}</p>
+              <p className="font-medium">{authorName}</p>
+              <p className="text-sm text-muted-foreground">{authorRole}</p>
             </div>
           </div>
         </div>
