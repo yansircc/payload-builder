14bf552a3a88180ba1740d42e4eb017179458c48
Bohdan-Subota <subota.legion1488@gmail.com>
2025-03-18 12:02:57 +0200

    feat: added functionality to set GTM ID using custon codes

--- src/app/(frontend)/next/scripts/GTM/GTM.tsx
+++ src/app/(frontend)/next/scripts/GTM/GTM.tsx
@@ -1,28 +1,33 @@
 import Script from 'next/script'
+import { getCustomCodes } from '@/utilities/getGTMId'
 
-const GTM = () => (
-  <>
-    <Script
-      id="gtm-script"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WZQM2J7H');`,
-      }}
-    ></Script>
+const GTM = async () => {
+  const { gtmId } = await getCustomCodes()
+  if (!gtmId) return null
 
-    <noscript>
-      <iframe
-        src="https://www.googletagmanager.com/ns.html?id=GTM-WZQM2J7H"
-        height="0"
-        width="0"
-        style={{ display: 'none', visibility: 'hidden' }}
-      ></iframe>
-    </noscript>
-  </>
-)
+  return (
+    <>
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${gtmId}');`,
+        }}
+      ></Script>
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
+    </>
+  )
+}
 
 export default GTM
---
+++ src/utilities/getGTMId.ts
@@ -0,0 +1,29 @@
+interface Script {
+  name: string
+  type: string
+  trackingId?: string
+  isEnabled: boolean
+  position: string
+  loadingStrategy: string
+  urlPattern: string
+  id: string
+}
+
+export const getCustomCodes = async () => {
+  try {
+    const res = await fetch(`${process.env.PAYLOAD_API_URL}/custom-codes?depth=0`, {
+      cache: 'no-store',
+    })
+    const data = await res.json()
+    console.log('data test', data)
+
+    const gtmScript = data?.docs?.find(
+      (script: Script) => script.type === 'google-tag-manager' && script.trackingId,
+    )
+
+    return { gtmId: gtmScript?.trackingId || null }
+  } catch (error) {
+    console.error('Failed to fetch GTM ID:', error)
+    return { gtmId: null }
+  }
+}
